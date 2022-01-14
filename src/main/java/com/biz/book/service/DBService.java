f4cf84890cab8296eb2bed94f07af7a18db1a157
Makrand <makrandshingare@gmail.com>
2022-01-14 21:58:46 +0530

    Added : Ability to update the Contact Information in the address book for a person

--- src/main/java/com/biz/book/service/DBService.java
+++ src/main/java/com/biz/book/service/DBService.java
@@ -10,15 +10,15 @@ public class DBService {
 
     DatabaseConnection databaseConnection = new DatabaseConnection();
 
-    public  boolean showData() {
+    public boolean showData() {
         List<ContactPerson> contactList = new ArrayList<>();
 
-        try (Connection connection =  databaseConnection.getConnection()){
+        try (Connection connection = databaseConnection.getConnection()) {
             Statement statement = connection.createStatement();
             String sqlQuery = "select * from address_book";
             ResultSet resultSet = statement.executeQuery(sqlQuery);
             while (resultSet.next()) {
-                ContactPerson info=new ContactPerson();
+                ContactPerson info = new ContactPerson();
                 info.setId(resultSet.getInt(1));
                 info.setFirstName(resultSet.getString(2));
                 info.setLastName(resultSet.getString(3));
@@ -32,10 +32,45 @@ public class DBService {
                 contactList.add(info);
             }
         } catch (SQLException throwables) {
-           return false;
+            return false;
         }
         System.out.println(contactList);
         return true;
     }
 
+    public boolean UpdateRecord(int id, String newName) {
+
+        try (Connection connection = databaseConnection.getConnection()) {
+            String sqlQuery = "update address_book set first_name= ? where id=?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+            preparedStatement.setString(1, newName);
+            preparedStatement.setInt(2, id);
+            int result = preparedStatement.executeUpdate();
+            if (result > 0) {
+                System.out.println("Record updated sucessfully...");
+            } else System.out.println("error");
+        } catch (Exception e) {
+
+        }
+        return true;
+    }
+
+    public String checkUpdatedRecord(int id) {
+        try (Connection connection = databaseConnection.getConnection()) {
+
+            String sqlQuery = "select first_name from address_book where id=" + id;
+            Statement statement = connection.prepareStatement(sqlQuery);
+
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+            while (resultSet.next()) {
+                return (resultSet.getString(1));
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return "Sad";
+
+    }
 }
--- src/test/java/AddressBookTest.java
+++ src/test/java/AddressBookTest.java
@@ -5,9 +5,19 @@ import org.testng.annotations.Test;
 public class AddressBookTest {
 
     @Test
-    public void MSTest() {
+    public void givenDBName_whenDBDataPresent_shouldPrintData() {
         DBService dbService=new DBService();
             boolean actualResult= dbService.showData();
         Assert.assertTrue(actualResult);
     }
+
+    @Test
+    public void givenFirstName_whenNameIspresnt_shouldUpdateRecord()
+    {
+        DBService dbService=new DBService();
+       dbService.UpdateRecord(1,"Ramesh");
+        String actualResult=dbService.checkUpdatedRecord(1);
+        String expectedResult="Ramesh";
+    }
+
 }
